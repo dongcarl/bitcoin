43b99b006cb303d66e82edf9cdf7dd6cb5f8fd6c
Carl Dong <accounts@carldong.me>
2019-03-11 13:36:53 -0400

    EXPERIMENTAL: Add convenience script for Guix builds

---
+++ contrib/guix/guix-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -ex
+
+# Download the depends sources now as we won't have internet access in the build
+# container
+make -C depends -j48 download
+
+# Hack: Remove '.la' files to avoid unnecessary dynamic linking
+sudo rm -f "$(guix environment --manifest=contrib/guix/manifest.scm --container --pure --no-grafts -- printenv LIBRARY_PATH)"/*.la
+
+# Run the build script 'contrib/guix/build.sh' in the build container specified
+# by 'contrib/guix/manifest.scm'
+guix environment --manifest=contrib/guix/manifest.scm --container --pure --no-grafts -- sh contrib/guix/build.sh
+
+# Hack: Remove rpaths from all binaries, and patch their interpreter
+for i in src/bitcoind src/bitcoin-cli src/bitcoin-tx src/qt/bitcoin-qt src/test/test_bitcoin
+do
+        patchelf --remove-rpath "$i"
+        interp_path="$(patchelf --print-interpreter "$i")"
+        patchelf --set-interpreter "/${interp_path#/*/*/*/}" "$i"
+done
